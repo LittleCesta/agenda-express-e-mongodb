01c3b9b9ef10b5f82802e8e00b170743ff029ce5
Cesar Romero <cesarromero34@gmail.com>
2026-04-26 12:28:26 -0300

    feat: implementa flash messages de erro e de sucesso

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,7 +1,8 @@
 exports.middlewareGlobal = (req, res, next) => {
   // Injetando pelo middleware eu consigo inserir em mais
   // de uma rota a mesma variável, sem precisar repetir o código
-  res.locals.umaVariavelLocal = "Este é o valor da variável local";
+  res.locals.errors = req.flash("errors");
+  res.locals.success = req.flash("success");
   next();
 };
 
